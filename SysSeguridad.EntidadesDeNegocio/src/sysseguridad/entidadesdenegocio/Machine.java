750e962ade1b18299a5cafad366b6dba732174aa
AlmasZ <not4lmas@gmail.com>
2023-08-10 21:37:26 -0600

    arreglando el modelo de machines

--- SysSeguridad.EntidadesDeNegocio/src/sysseguridad/entidadesdenegocio/Machine.java
+++ SysSeguridad.EntidadesDeNegocio/src/sysseguridad/entidadesdenegocio/Machine.java
@@ -1,11 +1,15 @@
 package sysseguridad.entidadesdenegocio;
-public class Machines {
+public class Machine extends Usuario{
     
     /**
+     * @param Object user es clase hija de Machine y es utilizada para traer el atributo nombre desde la otra clase
      * @param protected define que sera un atributo protegido que no cualquier clase podra acceder a el
-     * solo las clases hijas o heredadas, si en dado caso no se pueden guardar datos es necesario cambiar los protected a
+     * @solo las clases hijas o heredadas, si en dado caso no se pueden guardar datos es necesario cambiar los protected a
      * @param private para poder accceder sin menos restricciones
      */
+    
+    Usuario user = new Usuario();
+    
     protected int Id_Machines;
     protected String Machines_Name;
     protected String Brand;
@@ -13,13 +17,16 @@ public class Machines {
     protected String Acquisition_Date;
     protected String Maintenance_Date;
     protected String Next_Maintenance_Date;
+    protected byte Estatus;
+    protected String NameUser = user.getNombre();
     
     /**
      *@param public define el constructor para que pudea ser utilizado en otras clases xd
      *@param metodos se le pasan para que puedan ser ingresados los datos desde otra clase
+     * @since Machine se agrego NameUser que almacena el parametro de getNombre desde la otra clase.
      */
     
-    public Machines(int Id_Machines, String Machines_Name, String Brand, int Serial_Number, String Acquisition_Date, String Maintenance_Date, String Next_Maintenance_Date){
+    public Machine(int Id_Machines, String Machines_Name, String Brand, int Serial_Number, String Acquisition_Date, String Maintenance_Date, String Next_Maintenance_Date, Byte Estatus, String NameUser){
         this.Id_Machines = Id_Machines;
         this.Machines_Name = Machines_Name;
         this.Brand = Brand;
@@ -27,6 +34,8 @@ public class Machines {
         this.Acquisition_Date = Acquisition_Date;
         this.Maintenance_Date = Maintenance_Date;
         this.Next_Maintenance_Date = Next_Maintenance_Date;
+        this.Estatus = Estatus;
+        this.NameUser = NameUser;
     }
     
     /**
@@ -80,5 +89,30 @@ public class Machines {
     
     public void setNextMaintenanceDate(String Next_Maintenance_Date){
         this.Next_Maintenance_Date = Next_Maintenance_Date;
+    }
+    public String getNextMaintenanceDate(){
+        return Next_Maintenance_Date;
+    }
+    
+    public void setStatus(byte Status){
+        this.Estatus = Status;
+    }
+    public byte getStatus(){
+        return Estatus;
+    }
+ 
+    /**
+     *@param setNameUser probalemente sea eliminada ya que no es necesario utlizarlo aqui, ya no agregaremos nombres desde machines
+     * eso ya lo hace desde la clase usuario.
+     * @see por el momento se dejara por si en dado caso lanza conflictos
+     * @param getNameUser lo que hara es solamente traernos el dato de usuario ya que eso necesitamos
+     * 
+     */
+    @Deprecated
+    public void setNameUser(String NameUser){
+        this.NameUser = NameUser;
+    }
+    public String getNameUser(){
+        return NameUser;
     }
 }
